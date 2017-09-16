83c532a60500e27a88a53397efa3e81ca87fe9d4
Sam Noir <sreeni.nair@gmail.com>
2017-09-16 19:07:21 -0400

    Add all routes

--- flask_app.py
+++ flask_app.py
@@ -3,8 +3,31 @@
 
 from flask import Flask
 from flask import request
+from flask import jsonify
+
+
+class InvalidUsage(Exception):
+    status_code = 400
+    
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        if status_code:
+            self.status_code = status_code
+        self.payload = payload
+        
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv['message'] = self.message
+        return rv
 
 app = Flask(__name__)
+
+@app.errorhandler(InvalidUsage)
+def handle_invalid_usage(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
 
 @app.route('/')
 def hello_world():
@@ -16,4 +39,15 @@ def history():
     if request.method == 'POST':
         return 'POST is successful on /history!'
     elif request.method == 'GET':
+        tuid = request.args.get('tuid', None)
+        wuid = request.args.get('wuid', None)
+        if tuid:
+            print('TUID=', tuid)
+        if wuid:
+            print('WUID=', wuid)
+        if tuid and wuid:
+            raise(InvalidUsage(message='What am I? A teapot?', status_code=418))
         return 'GET received on /history!'
+    
+
+    
