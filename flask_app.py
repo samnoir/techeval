2bb916423e6c54b266b7b8f4efbd3c0ce849faa5
cheesydong <sreeni.nair@gmail.com>
2017-09-16 16:07:42 +0000

    Add a couple routes Tweak .gitignore for pyc

--- flask_app.py
+++ flask_app.py
@@ -2,6 +2,7 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
@@ -9,3 +10,10 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello from Flask!'
 
+@app.route('/history', methods=['GET', 'POST'])
+def history():
+    print(request.method)
+    if request.method == 'POST':
+        return 'POST is successful on /history!'
+    elif request.method == 'GET':
+        return 'GET received on /history!'
