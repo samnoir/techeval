199f887c8ddf9fad4a50f3cd90258b12e4734eac
Sam Noir <sreeni.nair@gmail.com>
2017-09-20 05:58:58 -0400

    Replace flask_pymongo with pymongo

--- flask_app.py
+++ flask_app.py
@@ -4,8 +4,12 @@
 from flask import Flask
 from flask import request
 from flask import jsonify
-from flask_pymongo import PyMongo
+#from flask_pymongo import PyMongo
+#from flask_pymongo import ASCENDING, DESCENDING
 import os
+import sys
+import pdb
+import pymongo
 
 
 class InvalidUsage(Exception):
@@ -31,7 +35,13 @@ app.config['MONGO_USERNAME'] = os.environ.get('MONGO_USERNAME') or 'rouser'
 app.config['MONGO_PASSWORD'] = os.environ.get('MONGO_PASSWORD') or 'ropw'
 
 # mongodb://<dbuser>:<dbpassword>@ds135624.mlab.com:35624/techeval
-mongo = PyMongo(app)
+#mongo = PyMongo(app)
+mongo = pymongo.MongoClient('mongodb://currentuser:currentpw@ds135624.mlab.com:35624/techeval')
+db = mongo.get_database()
+#app.config['MONGO_READ_DBNAME'] = 'techeval'
+#mongo_read = PyMongo(app, config_prefix='MONGO_READ') # Create a second Mongo object just for reads, to increase concurrency
+db.transactions.create_index([("timestamp", pymongo.DESCENDING), ("tuid", pymongo.DESCENDING)])
+db.transactions.create_index([("timestamp", pymongo.DESCENDING), ("ref_tuid", pymongo.DESCENDING)])
 
 @app.errorhandler(InvalidUsage)
 def handle_invalid_usage(error):
@@ -41,7 +51,13 @@ def handle_invalid_usage(error):
 
 @app.route('/')
 def hello_world():
+    #mongo.db.transactions.create_index([("timestamp", DESCENDING), ("tuid", DESCENDING)])
+    #mongo.db.transactions.create_index([("timestamp", DESCENDING), ("ref_tuid", DESCENDING)])
     return 'Hello from Flask!'
+
+def clean_results(x):
+    del(x['_id'])
+    return x
 
 @app.route('/history', methods=['GET', 'POST'])
 def history():
@@ -49,7 +65,7 @@ def history():
     if request.method == 'POST':
         data = request.get_json()
         try:
-            in_id = mongo.db.transactions.insert_one(data).inserted_id
+            in_id = db.transactions.insert_one(data).inserted_id
             return 'POST is successful on /history with id={}'.format(in_id)
         except:
             return 'POST is fail on /history!', 500
@@ -57,9 +73,13 @@ def history():
         tuid = request.args.get('tuid', None)
         wuid = request.args.get('wuid', None)
         if tuid:
-            print('TUID=', tuid)
+            print('GET with TUID=', tuid)
+            cur = db.transactions.find({'$or': [{"tuid": tuid}, {"ref_tuid": tuid}]}).sort("timestamp", pymongo.DESCENDING)
+            results = [clean_results(x) for x in cur]
+            return jsonify(results)
         if wuid:
             print('WUID=', wuid)
+            # Sort by tuid
         if tuid and wuid:
             raise(InvalidUsage(message='What am I? A teapot?', status_code=418))
         return 'GET received on /history!'
--- runapp.py
+++ runapp.py
@@ -1,2 +1,3 @@
 from flask_app import app
+#app.run(debug=False)
 app.run(debug=True)
